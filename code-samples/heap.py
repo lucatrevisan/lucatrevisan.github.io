25ab929d57c272105f944e885469c1cc1ceab964
Luca Trevisan <>
2021-02-24 11:38:09 +0100

    added comments to code and fixed a typo

--- code-samples/heap.py
+++ code-samples/heap.py
@@ -1,6 +1,8 @@
 class Heap:
 
     def __init__ (self,n):
+        #initializes empty data structure
+        #that can hold up to n elements
         self.max = n            # maximum size
         self.size = 0           # current size
         self.array = [0]*(n+1)  # array[i] has label of i-th element
@@ -15,13 +17,16 @@ class Heap:
         return self.array[ self.index[x] // 2 ]
         
     def is_leaf (self,x):
+        # returns True is x is a leaf, False otherwise
         return (self.index[x]*2 > self.size)
         
     def smaller_child (self,x):
+        # returns the label of the child of x with
+        # smallest key; behavior is undefined if x is a leaf
         ix = self.index[x]
         y = self.array[2*ix]
         if 2*ix+1 > self.size :
-            return x
+            return y
         else :
             z = self.array[2*ix + 1]
             if self.key[y] < self.key[z]:
@@ -44,6 +49,8 @@ class Heap:
 
 
     def remove_min(self):
+        #returns label of element of minimum key
+        #and removes it from data structure
         min = self.array[1]
         x = self.array[self.size]
         self.switch (min,x)
@@ -54,6 +61,7 @@ class Heap:
         return min
         
     def insert(self,x,k):
+        #inserts a new element of label x and key k
         self.size=self.size+1
         self.array[self.size]=x
         self.index[x]=self.size
@@ -63,6 +71,9 @@ class Heap:
 
         
     def decrease_key(self,x,k):
+        #changes key of element x to new, smaller, value k
+        #if k is bigger than previous value of self.key[x]
+        #the behavior is undefined
         self.key[x]=k
         while (self.index[x] >1) and (self.key[x]< self.key[self.parent(x)]):
             self.switch(x,self.parent(x))
